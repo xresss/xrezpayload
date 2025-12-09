68d95950f5df4da66336cf853243a73cbbd92add
xresss <xresxdigitalvpn@gmail.com>
2025-12-09 15:53:38 +0700

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,369 @@
+const data = {
+  // ===========================
+  //  XL
+  // ===========================
+  "XL": {
+    bugs: {
+      "EDUKASI": {
+        payload: [
+          "GET / HTTP/1.1[crlf]Host: edu.ruangguru.com[crlf][crlf]PATCH / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf][split]HTTP/ 1[crlf][crlf]",
+          "GET / HTTP/1.1[crlf]Host: edu.ruangguru.com[crlf][crlf]PATCH / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf][split]HTTP/ 69[crlf][crlf]"
+        ],
+        proxy: [
+          "104.17.70.206:80",
+          "ablink.alerts.udemy.com",
+          "104.17.3.81:80",
+          "104.22.4.240:80"
+        ]
+      },
+      
+      "CONFERENCE": {
+        payload: [
+          "GET / HTTP/1.1[crlf]Host: devsupport.zoom.us[crlf][crlf]PATCH / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf][split]HTTP/SweaterPink 200 OK[crlf][crlf]",
+          "PATCH /ssh-ws HTTP/1.1[crlf]Host: [host][crlf]Host: beta.zoom.us[crlf]Upgrade: websocket[crlf]Connection: Upgrade[crlf]User-Agent: @DINSTORE [ua][crlf][crlf]"
+        ],
+        proxy: [
+          "zoomcares.zoom.us:80",
+          "cdn-cf.zoom.us:80",
+          "support.zoom.us:80"
+        ]
+      },
+      
+      "UDEMY": {
+        payload: [
+          "GET / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: Websocket[crlf]Connection: Keep-Alive[crlf]Upgrade: websocket[crlf][crlf]",
+          "pake yg no satu aja"
+        ],
+        proxy: [
+          "www.udemy.com:80",
+          "pake yng no satu aja ya"
+        ]
+      },
+      
+      "VIDI": {
+        payload: [
+          "GET / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]",
+          "PKE NO 1"
+        ],
+        proxy: [
+          "104.18.1.196:80",
+          "PKE NO 1"
+        ]
+      },
+      
+      "tiktok": {
+        payload: [
+          "mode v2/vmmes",
+          "PKE NO 1"
+        ],
+        proxy: [
+          "api24-normal-alisg.tiktokv.com",
+          "api24-normal-useast1a.tiktokv.com",
+          "api24-normal.tiktokv.com"
+        ]
+      },
+      
+      "UTS": {
+        payload: [
+          "GET /cdn-cgi/trace HTTP/1.1[crlf]Host: ava.game.naver.com[crlf][crlf]BMOVE / [protocol][crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf][split]HTTP/1.1 200 OK[crlf][crlf]",
+          "PKE NO 1"
+        ],
+        proxy: [
+          "ava.game.naver.com sni graph.instagram.com",
+          "PKE NO 1"
+        ]
+      },
+      
+      "IFLIX": {
+        payload: [
+          "PAKE MODE SSL ONLY",
+          "PAKE MODE V2/VMMES BUG DI V2"
+        ],
+        proxy: [
+          "live.iflix.com",
+          "162.159.138.78"
+        ]
+      }
+    }
+  },
+  
+  // ===========================
+  // AXIS
+  // ===========================
+  "AXIS": {
+    bugs: {
+           "EDUKASI": {
+        payload: [
+          "GET / HTTP/1.1[crlf]Host: edu.ruangguru.com[crlf][crlf]PATCH / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf][split]HTTP/ 1[crlf][crlf]",
+          "GET / HTTP/1.1[crlf]Host: edu.ruangguru.com[crlf][crlf]PATCH / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf][split]HTTP/ 69[crlf][crlf]"
+        ],
+        proxy: [
+          "104.17.70.206:80",
+          "ablink.alerts.udemy.com",
+          "104.17.3.81:80",
+          "104.22.4.240:80"
+        ]
+      },
+      
+      "CONFERENCE": {
+        payload: [
+          "GET / HTTP/1.1[crlf]Host: devsupport.zoom.us[crlf][crlf]PATCH / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf][split]HTTP/SweaterPink 200 OK[crlf][crlf]",
+          "PATCH /ssh-ws HTTP/1.1[crlf]Host: [host][crlf]Host: beta.zoom.us[crlf]Upgrade: websocket[crlf]Connection: Upgrade[crlf]User-Agent: @DINSTORE [ua][crlf][crlf]"
+        ],
+        proxy: [
+          "zoomcares.zoom.us:80",
+          "cdn-cf.zoom.us:80",
+          "support.zoom.us:80"
+        ]
+      },
+      
+      "GAME": {
+        payload: [
+          "GET / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]",
+          "GET / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf]"
+        ],
+        proxy: [
+          "dev.appsflyer.com",
+          "web.poe.garena.com:80"
+        ]
+      },
+      
+      "SOSMED": {
+        payload: [
+          "GET http://v16.tiktokcdn.com/ HTTP/1.1\nHost: v16.tiktokcdn.com",
+          "CONNECT tiktok.com:443 HTTP/1.1\nHost: tiktok.com"
+        ],
+        proxy: [
+          "172.67.71.2:443",
+          "104.21.45.6:443"
+        ]
+      },
+      
+      "MUSIK": {
+        payload: [
+          "PATCH / HTTP/1.1[crlf]Host: [host][crlf]Host: zendesk1.spotify.com[crlf]Upgrade: websocket[crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf][crlf]",
+          "GET / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+        ],
+        proxy: [
+          "investors.spotify.com:80",
+          "investor.fb.com"
+        ]
+      },
+      
+      "SPOTIFY": {
+        payload: [
+          "HEAD /cdn-cgi/trace HTTP/1.1[crlf]Host: investors.spotify.com[crlf][crlf]PATCH / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf][split]HTTP/ 200[crlf][crlf]"
+        ],
+        proxy: [
+          "162.159.130.11"
+        ]
+      }
+    }
+  },
+  
+  // ===========================
+  // TELKOMSEL
+  // ===========================
+  "TELKOMSEL": {
+    bugs: {
+      "GAMEMAX": {
+        payload: [
+          "GET /cdn-cgi/trace HTTP/1.1[crlf]Host: cf-vod.nimo.tv[crlf][crlf]GET-RAY / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+        ],
+        proxy: [
+          "cf-vod.nimo.tv:80"
+        ]
+      },
+      
+      "ILMUPEDIA": {
+        payload: [
+          "GET /cdn-cgi/trace HTTP/1.1[crlf]Host: beta.zoom.us[crlf][crlf]GET-RAY / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]Content-Lenght: LUKI STORE[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]",
+          "CONNECT /cdn-cgi/trace HTTP/1.1[crlf]Host: ruangguru.com[crlf][crlf]GET-RAY / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: @DINSTOREVpn [ua][crlf]Upgrade: websocket[crlf][crlf]"
+        ],
+        proxy: [
+          "ruangguru.com:443",
+          "v2ray 104.20.28.42"
+        ]
+      }
+    }
+  },
+  
+  // ===========================
+  // INDOSAT
+  // ===========================
+  "Indosat": {
+    bugs: {
+      "APPS FUN 12GB": {
+        payload: [
+          "PACTH / HTTP/1.1[crlf]Host: [host][crlf]Host: blog.webex.com[crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+        ],
+        proxy: [
+          "141.193.213.20:80",
+          "creativeservices.netflix.com"
+        ]
+      },
+      
+      "APPS GAME 12GB": {
+        payload: [
+          "POSTT WSS://poki.com/ HTTP/1.1[crlf]Host: www.pubgmobile.com[crlf]User-Agent: @XRESXCONFIG [ua][crlf][crlf][split][cr]PATCH / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf]Connection: Upgrade[crlf][crlf]"
+        ],
+        proxy: [
+          "104.18.62.229:80"
+        ]
+      },
+      
+      "APPS EDUKASI 12gb": {
+        payload: [
+          "PACTH / HTTP/1.1[crlf]Host: [host][crlf]Host: api.webex.com[crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+        ],
+        proxy: [
+          "141.193.213.20:80"
+        ]
+      },
+      
+      "OPOK": {
+        payload: [
+          "GET /cdn-cgi/trace HTTP/1.1[crlf]Host: h.facebook.com[crlf]User-Agent: [ua][crlf]Referer: http://covid19.go.id[crlf][crlf]VERSION-CONTROL ws://[host] HTTP/1.1[crlf]Host: covid19.go.id[crlf]Upgrade: websocket[crlf][crlf]\n"
+        ],
+        proxy: [
+          "Covid19.go.id:80"
+        ]
+      }
+    }
+  },
+  
+  // ===========================
+  // BYU
+  // ===========================
+  "byu": {
+    bugs: {
+      "GGWP": {
+        payload: [
+          "GET /cdn-cgi/trace HTTP/1.1[crlf]Host: 104.19.143.108[crlf][crlf]CF-RAY / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: Websocket[crlf]Connection: Keep-Alive[crlf]User-Agent: @DINSTORE [ua][crlf]Upgrade: websocket[crlf][crlf]"
+        ],
+        proxy: [
+          "104.19.143.108",
+          "104.19.143.108:80"
+        ]
+      },
+      
+      "Opok": {
+        payload: [
+          "Payload  : GET / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+        ],
+        proxy: [
+          "space.byu.id:443 -SNI PKE HOST SSH"
+        ]
+      }
+    }
+  }
+  
+};
+//  ELEMENT
+// ===========================
+const operatorSelect = document.getElementById("operator");
+const bugSelect = document.getElementById("bug");
+const payloadList = document.getElementById("payloadList");
+const proxyList = document.getElementById("proxyList");  // ← DITAMBAHKAN
+const payloadOutput = document.getElementById("payloadOutput");
+const proxyOutput = document.getElementById("proxyOutput");
+
+
+// ===========================
+//  LOAD OPERATOR
+// ===========================
+function loadOperators() {
+  Object.keys(data).forEach(op => {
+    operatorSelect.innerHTML += `<option value="${op}">${op}</option>`;
+  });
+}
+
+
+// ===========================
+//  LOAD BUG SESUAI OPERATOR
+// ===========================
+operatorSelect.addEventListener("change", () => {
+  bugSelect.innerHTML = `<option value="">Pilih Bug / Kategori</option>`;
+  payloadList.innerHTML = `<option value="">Pilih Payload</option>`;
+  proxyList.innerHTML = `<option value="">Pilih Proxy</option>`;
+  payloadOutput.value = "Payload akan tampil di sini";
+  proxyOutput.value = "Proxy akan tampil di sini";
+
+  const op = operatorSelect.value;
+  if (!op) return;
+
+  Object.keys(data[op].bugs).forEach(bug => {
+    bugSelect.innerHTML += `<option value="${bug}">${bug}</option>`;
+  });
+});
+
+
+// ===========================
+//  LOAD PAYLOAD & PROXY LIST
+// ===========================
+bugSelect.addEventListener("change", () => {
+  payloadList.innerHTML = `<option value="">Pilih Payload</option>`;
+  proxyList.innerHTML = `<option value="">Pilih Proxy</option>`;
+  payloadOutput.value = "Payload akan tampil di sini";
+  proxyOutput.value = "Proxy akan tampil di sini";
+
+  const op = operatorSelect.value;
+  const bug = bugSelect.value;
+  if (!op || !bug) return;
+
+  // isi payload
+  data[op].bugs[bug].payload.forEach((p, i) => {
+    payloadList.innerHTML += `<option value="${i}">Payload ${i + 1}</option>`;
+  });
+
+  // isi proxy
+  data[op].bugs[bug].proxy.forEach((px, i) => {
+    proxyList.innerHTML += `<option value="${i}">Proxy ${i + 1}</option>`;
+  });
+});
+
+
+// ===========================
+//  TAMPILKAN PAYLOAD
+// ===========================
+payloadList.addEventListener("change", () => {
+  const op = operatorSelect.value;
+  const bug = bugSelect.value;
+  const idx = payloadList.value;
+
+  if (idx === "") return;
+
+  payloadOutput.value = data[op].bugs[bug].payload[idx];
+});
+
+
+// ===========================
+//  TAMPILKAN PROXY
+// ===========================
+proxyList.addEventListener("change", () => {
+  const op = operatorSelect.value;
+  const bug = bugSelect.value;
+  const idx = proxyList.value;
+
+  if (idx === "") return;
+
+  proxyOutput.value = data[op].bugs[bug].proxy[idx];
+});
+
+
+// ===========================
+//  COPY
+// ===========================
+function copyPayload() {
+  payloadOutput.select();
+  document.execCommand("copy");
+}
+
+function copyProxy() {
+  proxyOutput.select();
+  document.execCommand("copy");
+}
+
+
+// START
+loadOperators();
